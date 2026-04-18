50e9c1509bd4edbb98f757fba6870cb2ec8eaeea
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-04-18 13:57:39 +0800

    feat:(useProducts) add pagination

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,35 +1,67 @@
 import { useState, useEffect } from "react";
 import type { Product } from "../types/product";
+
 const API_URL = import.meta.env.VITE_API_URL;
+
+interface Meta {
+  page: number;
+  limit: number;
+  total: number;
+  totalPages: number;
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+}
 
 const useProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [meta, setMeta] = useState<Meta | null>(null);
+
+  const [page, setPage] = useState(1);
+  const [limit] = useState(10);
+
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await fetch(`${API_URL}/api/products`);
+  const fetchProducts = async () => {
+    try {
+      setLoading(true);
 
-        if (!res.ok) {
-          throw new Error("Failed to fetch");
-        }
+      const res = await fetch(
+        `${API_URL}/api/products?page=${page}&limit=${limit}`,
+      );
 
-        const data: Product[] = await res.json();
-        setProducts(data.data);
-      } catch (error) {
-        setError("Failed to fetch products");
-        console.error(error);
-      } finally {
-        setLoading(false);
+      if (!res.ok) {
+        throw new Error("Failed to fetch");
       }
-    };
 
+      const data = await res.json();
+
+      setProducts(data.data);
+      setMeta(data.meta);
+    } catch (error) {
+      setError("Failed to fetch products");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [page, limit]);
 
-  return { products, setProducts, loading, error };
+  return {
+    products,
+    setProducts,
+    loading,
+    error,
+
+    // 🔥 NEW
+    page,
+    setPage,
+    meta,
+    refetch: fetchProducts,
+  };
 };
 
 export default useProducts;
